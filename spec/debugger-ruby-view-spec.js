c9a1b14d5dbf39408709819dcd0ffb0c279c515a
Andrei Moldovan <andrei@meisterlabs.com>
2016-04-16 12:06:39 +0200

    Initial commit

---
+++ spec/debugger-ruby-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DebuggerRubyView from '../lib/debugger-ruby-view';
+
+describe('DebuggerRubyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
